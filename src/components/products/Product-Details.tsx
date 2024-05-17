69260be11f7980a87ebcb4ae92e6a83a79402a78
bereczzsolt <zsolti0302270@gmail.com>
2024-05-17 16:13:58 +0200

    Design update

--- src/components/products/Product-Details.tsx
+++ src/components/products/Product-Details.tsx
@@ -1,44 +1,68 @@
 import React, { FC } from 'react';
 import { Product } from '../../model';
-import { Box, Flex, AspectRatio, Image, HStack, Heading, Text, } from '@chakra-ui/react';
-
-
+import { Box, Flex, AspectRatio, Image, VStack, Heading, Text, Badge, Button } from '@chakra-ui/react';
+import { FaStar } from "react-icons/fa";
 
 export interface ProductDetailsProps {
     product: Product;
 }
 
 export const ProductDetails: FC<ProductDetailsProps> = ({ product }) => {
+    const renderStars = () => {
+        const stars = [];
+        for (let i = 1; i <= product.rating; i++) {
+            stars.push(<FaStar key={i} />);
+        }
+        return stars;
+    };
     return (
-        <Box as="article" bgColor="background.dark" pb="7" pt="9" px="16">
-            <Flex justifyContent="space-between" gap={10}>
-                <AspectRatio ratio={323 / 486} flexBasis="30%" flexShrink={0}>
-                    <>
-                        <Image src={product.image} />
-                        <Box flexGrow={1}>
-                            <Box as="header" marginBottom={8}>
-                                <HStack spacing={6}>
-                                    <Heading fontSize="4xl" textTransform="uppercase">{product.name}</Heading>
-                                </HStack>
-
-                            </Box>
-                            <HStack as="time" spacing={12} color="text.highlighted" fontSize="2xl" mb="7">
-                                <Text as="span"> {product.rating}</Text>
-                                <Text as="span">{product.stock}</Text>
-                            </HStack>
-                            <Text fontSize="xl" opacity={0.5}>{product.description}</Text>
-                        </Box>
-                    </>
+        <Box 
+            as="article" bgColor="gray.900"  p="8" 
+            borderWidth="1px" borderRadius="xl" 
+            maxWidth="1000px" mx="auto" 
+            boxShadow="2xl" overflow="hidden"
+        >
+            <Flex 
+                direction={{ base: "column", md: "row" }} 
+                justifyContent="space-between" 
+                gap={10}
+                alignItems="flex-start"
+            >
+                <AspectRatio 
+                    ratio={323 / 486} 
+                    flexBasis="30%" flexShrink={0}
+                    borderRadius="lg" overflow="hidden" boxShadow="lg"
+                >
+                    <Image src={product.image} objectFit="cover" />
                 </AspectRatio>
-
-
-
+                <Box 
+                    flexGrow={1} p={6} 
+                    bg="gray.800" borderWidth="1px" borderRadius="lg" boxShadow="lg"
+                    color="white"
+                >
+                    <VStack align="flex-start" spacing={5}>
+                        <Box as="header">
+                            <Heading fontSize="4xl" textTransform="uppercase" color="teal.300">{product.name}</Heading>
+                        </Box>
+                        <VStack spacing={3} align="flex-start" color="teal.200">
+                        <Flex align="center">
+                            <Badge colorScheme="teal" fontSize="lg" mr="2">Értékelés:</Badge>
+                                <Flex>{renderStars()}</Flex>
+                        </Flex>
+                            <Badge colorScheme="orange" fontSize="lg">Raktáron: {product.stock}</Badge>
+                        </VStack>
+                        <Text fontSize="lg" lineHeight="tall" textAlign="justify">{product.description}</Text>
+                        <Button 
+                            mt={4}
+                            size="lg"
+                            colorScheme="teal"
+                            variant="solid"
+                        >
+                            Add to Cart
+                        </Button>
+                    </VStack>
+                </Box>
             </Flex>
         </Box>
-
-
-
-
-
     );
 };
